a58d52146a0e8624647485d503cbb3d72ebaf69f
suniledureka <sunil.edureka@gmail.com>
2025-07-07 11:12:36 +0530

    day-6

--- day-5/codes/06.sb-data-generators/src/main/java/com/cognizant/boot/data/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.boot.data.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cognizant.boot.data.entities.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-}
