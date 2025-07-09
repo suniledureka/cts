3c4f593446b5b979393ed0524a12686b2d09067e
suniledureka <sunil.edureka@gmail.com>
2025-07-09 17:25:51 +0530

    day-9

--- day-8/codes/11.irctc-app-db-except/src/main/java/com/cognizant/irctc/repository/TicketRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.irctc.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cognizant.irctc.models.Ticket;
-
-public interface TicketRepository extends JpaRepository<Ticket, String> {
-
-}
