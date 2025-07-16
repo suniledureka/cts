641ab326134f1fe64025b0b034c98c36f1c0283c
suniledureka <sunil.edureka@gmail.com>
2025-07-16 12:31:45 +0530

    day-14

---
+++ day-14/codes/util/StringMethodAgain.java
@@ -0,0 +1,14 @@
+package com.capgemini.java.util;
+
+public class StringMethodAgain {
+
+	public static void main(String[] args) {
+		String str1 = "ABC";
+		//String str2 = "ABC";
+		//String str2 = "ADA";
+        String str2 = "ABA";
+        
+		int n = str1.compareTo(str2);
+		System.out.println(n);
+	}
+}
